a4ca6d0cc70a3866cc77f2b817a0c8b8e3d697e5
3manu31C0dium <eguerra.urrea@udea.edu.co>
2024-08-26 23:14:59 -0500

    Añadir proyecto practica 1 (qtcreator)

---
+++ Practica1/punto2.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+//no funciona, hay que solucionarlo luego.
+
+
+int main()
+{
+
+    int A ;
+    cout<< "Ingrese un numero" << endl;
+    cin >> A;
+
+    if(A&&2==0){
+        cout<<"El numero es par." << endl;
+    }
+
+    else{
+
+        cout << "El numero es impar."<< endl;
+                 };
+}
